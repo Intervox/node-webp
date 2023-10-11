806daee34f4b7837cccf2d30877b1bb9efadaa1a
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2023-10-11 19:36:07 +0200

    Load package.json using require

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const fs = require('fs');
 const { CWebp, DWebp } = require('./webp');
 
-const pkg = path.resolve(__dirname, '../package.json');
-const { version } = JSON.parse(fs.readFileSync(pkg, 'utf8'));
+const { version } = require('../package.json');
 
 module.exports = Object.assign(CWebp, { CWebp, DWebp, version });
